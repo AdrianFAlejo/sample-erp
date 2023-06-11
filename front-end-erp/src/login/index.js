7e8d9a5ceb2292de4e4503564ed3421860934352
Adrian Alejo <adrian.alejo@hoolisoftware.com>
2023-06-11 17:28:42 +0800

    added backend folder

---
+++ front-end-erp/src/login/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { AppBar, Box, Button, CircularProgress, Link, Toolbar, Typography, Divider, Paper, Modal, TextField } from '@material-ui/core';
+
+
+
+const SignIn = () => {
+    return (
+        <div>
+            <h1>Adrianzzz</h1>
+        </div>
+        // <Box>
+
+        // </Box>
+    )
+
+}
+
+export default SignIn;
